9053cd4f0e2c888ea7c716ca69e2b79f2a172836
Bilgehan <b.akcan013@gmail.com>
2023-08-20 19:57:00 +0300

    final commit

--- src/main/java/com/example/demo/flightSearch/AirportController.java
+++ src/main/java/com/example/demo/flightSearch/AirportController.java
@@ -2,8 +2,8 @@ package com.example.demo.flightSearch;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+
 @RestController
 @RequestMapping(path = "api/v1/airports")
 public class AirportController {
